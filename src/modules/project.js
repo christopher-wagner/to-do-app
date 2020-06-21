f932b844042c69e2ae8b57529c05b41a2d8ec49c
Chris Wagner <c.wagner7443@gmail.com>
2020-06-21 10:43:26 -0500

    Able to use Prompt to get user's Projects with Todo items and display to DOM

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,25 +1,62 @@
+import ToDo from './todo.js'
+
 
 const Project = (pTitle, pDescription) => {
     pTitle = pTitle
     pDescription = pDescription
     let toDoPList = []
+
+    const createToDoItem = () => {
+        const todoitem = ToDo(prompt(),prompt() , prompt(), prompt())
+        toDoPList.push(todoitem)
+    }
+
     const returnPTitle = () => pTitle
     const returnPDesc = () => pDescription
     const returnPToDoList = () => toDoPList.map(
         todoitem => console.log(todoitem.returnTitle())
     )
 
+    const returnPtodoTitle = (todoitem) => {
+        todoitem.returnTitle()
+    }
+
     const addToDoItemToList = (todoitem) => {
         toDoPList.push(todoitem)
     }
 
+    const addTODOItemToDOM = () => {
+        const container = document.getElementById("container")
+        const DOMCONSOLE = document.createElement('h1')
+        const TODOContainer = document.createElement("div")
+        const title = document.createElement('div')
+        const description = document.createElement('div')
+        const dueDate = document.createElement('div')
+        const priority = document.createElement('div')
 
+        DOMCONSOLE.innerHTML = "TODO Item"
+        title.innerHTML = toDoPList[0].returnTitle()
+        description.innerHTML = toDoPList[0].returnDesc()
+        dueDate.innerHTML = toDoPList[0].returnDueDate()
+        priority.innerHTML = toDoPList[0].returnPriority()
+
+        TODOContainer.appendChild(DOMCONSOLE)
+        TODOContainer.appendChild(title)
+        TODOContainer.appendChild(description)
+        TODOContainer.appendChild(dueDate)
+        TODOContainer.appendChild(priority)
+
+        container.appendChild(TODOContainer)
+    }
 
     return {
+        createToDoItem,
         returnPTitle,
         returnPDesc,
-        addToDoItemToList,
         returnPToDoList,
+        returnPtodoTitle,
+        addToDoItemToList,
+        addTODOItemToDOM
     }
 }
 
--- src/modules/projectHolder.js
+++ src/modules/projectHolder.js
@@ -1,18 +1,51 @@
-
+import Project from './project.js'
 
 const ProjectHolder = () => {
 
     let projectHolderList = []
 
-    const returnProjectHolder = () => projectHolderList.map(
+    const createProject = () => {
+        const projectItem = Project(prompt(), prompt())
+        projectHolderList.push(projectItem)
+        projectItem.createToDoItem()
+        projectItem.addTODOItemToDOM()
+    }
+
+    const returnProjectHolderItem = () => projectHolderList.map(
         projectItem => console.log(projectItem.returnPTitle())
     )
     const addProjectItemToList = (projectItem) => {
         projectHolderList.push(projectItem)
     }
+
+    const addProjectItemToDOM = () => {
+        const container = document.getElementById("container")
+        const DOMCONSOLE = document.createElement('h1')
+        const projectContainer = document.createElement("div")
+        const title = document.createElement('div')
+        const description = document.createElement('div')
+
+        DOMCONSOLE.innerHTML = "Project"
+        title.innerHTML = projectHolderList[0].returnPTitle()
+        description.innerHTML = projectHolderList[0].returnPDesc()
+
+        projectContainer.appendChild(DOMCONSOLE)
+        projectContainer.appendChild(title)
+        projectContainer.appendChild(description)
+
+        container.appendChild(projectContainer)
+    }
+
+    const callProjectToDoTitles = (projectItem) => {
+        projectItem.returnPToDoList(projectHolderList[0])
+    }
+    
     return {
-        returnProjectHolder,
-        addProjectItemToList
+        createProject,
+        returnProjectHolderItem,
+        addProjectItemToList,
+        addProjectItemToDOM,
+        callProjectToDoTitles
     }
 }
 
